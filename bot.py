1b52178aca8238f889bd19cf1d3978d1a729beae
javidelrojoo <javitau459@gmail.com>
2020-10-20 23:16:50 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ async def on_ready():
 	print('Estoy Listo')
 
 @tasks.loop(seconds=10)
-def change_status():
+async def change_status():
 	await client.change_presence(activity=discord.Game(next(status)))
 
 @client.command()
