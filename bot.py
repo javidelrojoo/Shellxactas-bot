c738f1f7b6c1813f8f61bf7ebcaba55166d31b91
javidelrojoo <javitau459@gmail.com>
2020-10-21 13:04:57 -0300

    dolar

--- bot.py
+++ bot.py
@@ -113,10 +113,11 @@ async def dolar(ctx):
     if dlr.valor_dolar_blue()==-1:
         await ctx.send('Ocurrió un error.')
         return
-    compra,venta=dlr.valor_dolar_blue()
+    compra,venta,act=dlr.valor_dolar_blue()
     embedVar = discord.Embed(title="Precio Dolar",url="https://www.dolarhoy.com/cotizaciondolarblue",color=0x0400ff)
     embedVar.add_field(name="Compra", value=f"${compra}", inline=False)
     embedVar.add_field(name="Venta", value=f"${venta}", inline=False)
+	embedVar.set_footer(text=f"Última actualizacion: {act}")
     await ctx.send(embed=embedVar)
 
 @client.command()
--- dolar.py
+++ dolar.py
@@ -8,17 +8,18 @@ def get_float(text):
 	float_value = float(re.search(float_regex, text)[0])
 	return float_value
 
-
 def valor_dolar_blue():
 	req = requests.get('https://www.dolarhoy.com/cotizaciondolarblue').text
 	soup = bs4.BeautifulSoup(req, 'html.parser')
 	valor_dolar_container = soup.find('div', class_ = 'venta')
 	valor_c=soup.find('div', class_ = 'compra')
+	act=soup.find('span',class_='update')
 
 	if valor_dolar_container != None:
 		valor_venta = get_float(valor_dolar_container.text)
 		valor_compra=get_float(valor_c.text)
-		return valor_compra,valor_venta
+		return valor_compra,valor_venta,act.text
 
 	else:
 		return -1
+print(valor_dolar_blue())
