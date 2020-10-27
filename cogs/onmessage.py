a5657d00dce7ad566701252b5aa2fd8716e0d4e2
javidelrojoo <javitau459@gmail.com>
2020-10-26 22:05:20 -0300

    Update onmessage.py

--- cogs/onmessage.py
+++ cogs/onmessage.py
@@ -9,6 +9,13 @@ class Mensajes(commands.Cog):
         self.client = client
 
     @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+            await ctx.send('¿Necesitas ayuda para escribir el comando?')
+            return
+        raise error
+    
+    @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
