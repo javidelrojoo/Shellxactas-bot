04612ea9415200fc4cf845a6892c8efbedf0d26e
javidelrojoo <javitau459@gmail.com>
2020-10-30 19:47:19 -0300

    Update loops.py

--- cogs/loops.py
+++ cogs/loops.py
@@ -26,7 +26,7 @@ class Loops(commands.Cog):
         self.client = client
         self.campus_loop.start()
         self.change_status.start()
-        self.loop.create_task(self.upremindme())
+        self.client.loop.create_task(self.upremindme())
 
     @tasks.loop(seconds=60.0)
     async def campus_loop(self):
