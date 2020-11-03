4bdde8418e71c4dc6af2d766b797b290e2e2464b
javidelrojoo <javitau459@gmail.com>
2020-11-03 16:52:54 -0300

    Quito la carpeta img

--- img_editor.py
+++ img_editor.py
@@ -1,14 +1,14 @@
 from PIL import Image
 import numpy as np
-
-
+import requests
+from io import BytesIO
 
 
 def get_image_size(image):
     return image.size
 
 
-def resize_image(image, size:tuple):
+def resize_image(image, size: tuple):
     resized_image = image.resize(size)
     return resized_image
 
@@ -20,9 +20,12 @@ def read_image(path):
     except Exception as e:
         print(e)
 
+
 def picardia_overlay(img_path):
     img = read_image(img_path)
-    picardia = read_image('img/picardia.png')
+
+    picardia_url = requests.get('https://i.imgur.com/dJq0f5F.png')
+    picardia = Image.open(BytesIO(picardia_url.content))
 
     img = resize_image(img, get_image_size(picardia))
 
