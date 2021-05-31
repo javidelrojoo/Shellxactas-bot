1b2ca37187337dda63bc1ae6862eda83393edef7
javidelrojoo <javitau459@gmail.com>
2021-05-31 15:57:14 -0300

    callate puta

--- cogs/onmessage.py
+++ cogs/onmessage.py
@@ -77,6 +77,9 @@ class Mensajes(commands.Cog):
         
         if 'jaja' in message.content.lower() and message.author.id == 452285420609339402:
             await message.add_reaction('<:joaco:788188614328713217>')
+        
+        if ('ya lo pasé' in message.content.lower() or 'ya lo pase' in message.content.lower() or 'ya lo pasaron' in message.content.lower()) and message.author.id == 697124888879693895:
+            await message.send('Callate puta')
 
         if 'basado' in message.content.lower() or 'based' in message.content.lower(): 
             await message.add_reaction("<:BASED:783045382426722344>")
